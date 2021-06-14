6d858a9f98ba3a6ec61514fc30a5048647044c84
Kristof <kristof.kovacs@telenav.com>
2021-06-14 22:19:09 +0300

    Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name='selenium_chrome',
-    version='0.0.15',
+    version='0.0.16',
     author='Kristóf-Attila Kovács',
     description='selenium_chrome',
     long_description=long_description,
