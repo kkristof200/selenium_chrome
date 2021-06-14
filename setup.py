43aad2226f8b405129db6b574cfc496dad257a92
Kristof <kristof.kovacs@telenav.com>
2021-06-14 18:44:40 +0300

    Update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ else:
 
 setuptools.setup(
     name='selenium_chrome',
-    version='0.0.2',
+    version='0.0.3',
     author='Kristóf-Attila Kovács',
     description='selenium_chrome',
     long_description=long_description,
@@ -19,12 +19,12 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'geckodriver-autoinstaller>=0.1.0',
-        'kproxy>=0.0.2',
-        'noraise>=0.0.26',
+        'kproxy>=0.0.1',
+        'noraise>=0.0.16',
         'selenium>=3.141.0',
-        'selenium-browser>=0.0.21',
+        'selenium-browser>=0.0.11',
         'undetected-chromedriver>=3.0.1',
-        'xpath-utils>=0.0.2'
+        'xpath-utils>=0.0.3'
     ],
     classifiers=[
         'Programming Language :: Python :: 3.4',
